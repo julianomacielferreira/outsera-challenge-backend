9e76899668ce60a11ec0999dd02e0cd8f421d926
Juliano Maciel <ju.maciel.ferreira@gmail.com>
2025-10-01 18:04:24 -0300

    Create integration tests.

--- src/test/java/com/outsera/api/ApplicationTests.java
+++ src/test/java/com/outsera/api/ApplicationTests.java
@@ -23,14 +23,66 @@
  */
 package com.outsera.api;
 
+import com.outsera.api.movies.Movie;
+import com.outsera.api.movies.MovieRepository;
+import com.outsera.api.movies.MovieService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MovieService movieService;
 
+    @Autowired
+    private MovieRepository movieRepository;
+
+
+    @BeforeEach
+    public void setup() {
+        movieRepository.deleteAll();
+
+        Movie movie1 = new Movie();
+        movie1.setYear(2011);
+        movie1.setTitle("Jack and Jill");
+        movie1.setStudios("Columbia Pictures");
+        movie1.setProducers("Jack Giarraputo and Adam Sandler");
+        movie1.setWinner(true);
+        movieRepository.save(movie1);
+
+        Movie movie2 = new Movie();
+        movie2.setYear(2013);
+        movie2.setTitle("Grown Ups 2");
+        movie2.setStudios("Columbia Pictures");
+        movie2.setProducers("Jack Giarraputo and Adam Sandler");
+        movie2.setWinner(true);
+        movieRepository.save(movie2);
+    }
+
+    @Test
+    public void testGetWinnerProducers() {
+        Map<String, List<MovieService.ProducerInterval>> result = movieService.getMovieProducers();
+
+        assertNotNull(result);
+        assertTrue(result.containsKey("min"));
+        assertTrue(result.containsKey("max"));
+
+        List<MovieService.ProducerInterval> minIntervals = result.get("min");
+        assertNotNull(minIntervals);
+        assertEquals(1, minIntervals.size());
+
+        MovieService.ProducerInterval minInterval = minIntervals.get(0);
+        assertEquals("Jack Giarraputo and Adam Sandler", minInterval.getProducer());
+        assertEquals(2, minInterval.getInterval());
+        assertEquals(2011, minInterval.getPreviousWin());
+        assertEquals(2013, minInterval.getFollowingWin());
+    }
 }
