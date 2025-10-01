48e1c0a05b6d708671e50ff92f9a1f549032c0ca
Juliano Maciel <ju.maciel.ferreira@gmail.com>
2025-10-01 16:01:57 -0300

    Rename files.

--- src/test/java/com/outsera/api/ApplicationTests.java
+++ src/test/java/com/outsera/api/ApplicationTests.java
@@ -27,7 +27,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
