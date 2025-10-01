e9a4559f27e392ae74ed82e9f761f249b7b21d9b
Juliano Maciel <ju.maciel.ferreira@gmail.com>
2025-10-01 16:07:47 -0300

    Create Movie service to populate the table.

---
+++ src/main/java/com/outsera/api/movies/MovieService.java
@@ -0,0 +1,65 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2025 juliano.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.outsera.api.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public void loadMoviesFromCSV(MultipartFile file) throws IOException {
+
+        try (BufferedReader buffer = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
+
+            String line;
+
+            buffer.readLine();
+
+            while ((line = buffer.readLine()) != null) {
+                String[] data = line.split(";");
+
+                Movie movie = new Movie();
+                movie.setYear(Integer.parseInt(data[0]));
+                movie.setTitle(data[1]);
+                movie.setStudios(data[2]);
+                movie.setProducers(data[3]);
+
+                if (data.length > 4 && data[4] != null) {
+                    movie.setWinner(true);
+                }
+                
+                movieRepository.save(movie);
+            }
+        }
+    }
+}
